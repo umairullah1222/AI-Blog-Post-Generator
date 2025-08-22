6e109f2a1aabbc58460c00a87dfcef1d042f05b8
Umairullah Ramzan <umairullahramzan189945@gmail.com>
2025-08-22 04:00:42 -0700

    Describe your changes

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AppProvider } from './contexts/AppContext.tsx';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
